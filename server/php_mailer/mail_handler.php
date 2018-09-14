6cb3714d88de34a733c69f014fa617376352042b
Simon Hoblik <SimonHoblik@simons-air.attlocal.net>
2018-09-14 12:23:33 -0700

    redesign of the launch demo screen

---
+++ server/php_mailer/mail_handler.php
@@ -0,0 +1,71 @@
+<?php
+require_once('email_config.php');
+require_once('phpmailer/PHPMailer/PHPMailerAutoload.php');
+
+$postJSON = file_get_contents('php://input');
+$post = json_decode($postJSON, TRUE);
+
+$output = [
+  'success' => false,
+];
+    $mail = new PHPMailer;
+    $mail->SMTPDebug = 0;           // Enable verbose debug output. Change to 0 to disable debugging output.
+
+    $mail->isSMTP();                // Set mailer to use SMTP.
+    $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
+    $mail->SMTPAuth = true;         // Enable SMTP authentication
+
+
+    $mail->Username = EMAIL_USER;   // SMTP username
+    $mail->Password = EMAIL_PASS;   // SMTP password
+    $mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+    $mail->Port = 587;              // TCP port to connect to
+    $options = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    $mail->smtpConnect($options);
+
+    if (isset($verifyAccount)) {
+      $mail->From = 'kodwizmessage@gmail.com';  // sender's email address (shows in "From" field)
+      $mail->FromName = 'KodWiz';   // sender's name (shows in "From" field)
+      $mail->addAddress($post['email']);  // Add a recipient
+
+      $mail->isHTML(true);
+
+      $mail->Subject = 'Verify your Account';
+      $mail->Body    = 'Thank you for signing up with Kod Wiz please follow the link to activate your account ' . $verifyLink;
+
+    } else {
+      $mail->From = 'kodwizmessage@gmail.com';  // sender's email address (shows in "From" field)
+      $mail->FromName = 'KodWiz';   // sender's name (shows in "From" field)
+      $mail->addAddress('shoblik@yahoo.com');  // Add a recipient
+      $mail->addAddress('jhoblik@yahoo.com');
+      $mail->addAddress('otto.oficial@gmail.com');
+
+      $mail->addReplyTo($post['email']);
+      $mail->isHTML(true);
+
+      $mail->Subject = 'Kod Wiz Company Site Message';
+      $mail->Body    = $post['message'];
+    }
+    $mail->AltBody = htmlentities('something went wrong');
+
+    if(!$mail->send()) {
+        $output['success'] = false;
+        $output['message'] = $mail->ErrorInfo;
+    } else {
+//    echo 'Message has been sent';
+        $output['emailSent'] = true;
+        $output['success'] = true;
+
+    }
+    if (!isset($verifyAccount)) {
+      $output = json_encode($output);
+      print($output);
+    }
+
+?>
