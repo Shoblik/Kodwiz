d82a59c285521643a62d5bdbdb0ff98525a0e9e6
Simon Hoblik <SimonHoblik@simons-air.attlocal.net>
2018-09-14 12:11:57 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,83 @@
+function submitForm(e) {
+  e.preventDefault();
+  console.log('submit form');
+  axios({
+  method: 'post',
+  url: './server/php_mailer/mail_handler.php',
+  data: {
+    message: document.getElementById('message').value,
+    email: document.getElementById('email').value
+  }
+}).then(function(response) {
+  console.log(response);
+  let target = document.getElementById('feedbackMessage');
+  if (response.data.emailSent) {
+    target.innerHTML = 'Thank you for contacting us, we\'ll be reaching out to you soon.';
+    target.style.display = 'inline-block';
+} else  {
+    target.innerHTML = 'Something went wrong please contact us at jhoblik@kod-wiz.com or at 714-608-7664';
+    target.style.display = 'inline-block';
+  }
+});
+}
+function showHeader() {
+  document.getElementById('home').style.opacity = 1;
+}
+function smoothScroll(destination) {
+  //element to scroll to
+  let target = document.getElementById(destination);
+
+  event.preventDefault();
+  window.scrollTo({
+    top: target.getBoundingClientRect().top + window.scrollY,
+    behavior: "smooth"
+  });
+}
+
+window.addEventListener('scroll', function() {
+  let elementArr = ['.singleAttribute', '.tutorialBtn', '#contactInstructions', '.formContainer'];
+  function isScrolledIntoView() {
+    let data = [];
+    let offset = 100;
+    for (i = 0; i < elementArr.length; i++) {
+      let eleSelector = document.querySelector(elementArr[i]);
+      if (eleSelector) {
+        var rect = eleSelector.getBoundingClientRect();
+        var elemTop = rect.top + offset;
+        var elemBottom = rect.bottom + offset;
+
+        isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+        let temp = {
+          element: eleSelector,
+          isVisible: isVisible,
+          selector: elementArr[i],
+        }
+        data.push(temp);
+      }
+  }
+  return data;
+}
+
+  (function() {
+    let data = isScrolledIntoView();
+    for (let i in data) {
+      if (data[i].isVisible && data[i].selector === '.singleAttribute') {
+        let divs = document.querySelectorAll('.singleAttribute');
+        let timeout = 0;
+        for (let x = 0; x < divs.length; x++) {
+          setTimeout(function() {
+            divs[x].style.transform = 'none';
+            divs[x].style.opacity = 1;
+            console.log(timeout)
+          }, timeout);
+          timeout += 300;
+        }
+        return;
+      } else if (data[i].isVisible) {
+        data[i].element.style.transform = 'none';
+        data[i].element.style.opacity = 1;
+      }
+    }
+  })();
+
+});
---
+++ server/database_connect/actions/register.php
@@ -0,0 +1,54 @@
+<?php
+if (isset($ACCESS_CONTROL)) {
+  $output['success'] = true;
+
+  //check if the email is already in the system
+  $duplicateQuery = "SELECT * FROM `customer` WHERE `email` = '{$post['email']}'";
+  $duplicateResult = mysqli_query($conn, $duplicateQuery);
+
+  $data['num_rows'] = $duplicateResult->num_rows;
+
+  if ($duplicateResult->num_rows === 0) {
+    $query = "INSERT INTO `customer` (`first_name`, `last_name`, `bussiness`, `email`, `phone`, `pin`, `date_added`, `active`)
+    VALUES ('{$post['first_name']}', '{$post['last_name']}', '{$post['bussiness']}', '{$post['email']}', '{$post['phone']}', '{$post['pin']}', CURRENT_DATE, '{$post['active']}')";
+
+    $result = mysqli_query($conn, $query);
+    $output['customer_added'] = $result;
+
+    // generate code in verify customer column
+    $insert_id = $conn->insert_id;
+    $code = random_str(15);
+
+    $query = "INSERT INTO `verify_customer` (`customer_id`, `confirmation_code`)
+              VALUES ('$insert_id', '$code')";
+    $result = mysqli_query($conn, $query);
+
+    $output['customer_verification_added'] = true;
+
+    //send email to customer with link to follow to verify account
+    $verifyAccount = true;
+    if ($_SERVER['HTTP_HOST'] === 'localhost') {
+      $verifyLink = "<a href='http://localhost/server/database_connect/server.php?action=get&resource=verify&code=$code'>here</a>";
+    } else {
+      $verifyLink = "<a href='https://kodwiz.com/server/database_connect/server.php?action=get&resource=verify&code=$code'>here</a>";
+    }
+
+    require_once('../php_mailer/mail_handler.php');
+
+  } else {
+    $output['errors'] = 'This email is already in use';
+  }
+
+} else {
+  die('no direct access allowed');
+}
+
+function random_str($length, $keyspace = '0123456789')
+{
+    $pieces = [];
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $pieces []= $keyspace[random_int(0, $max)];
+    }
+    return implode('', $pieces);
+}
