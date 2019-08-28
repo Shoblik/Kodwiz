3d6061d24f943de8acf81049252cfd76da8de50f
= <simon@shoblik@yahoo.com>
2019-08-28 15:45:11 -0700

    viewport change

--- tutorials/index.php
+++ tutorials/index.php
@@ -11,7 +11,7 @@ $videos = mysqli_fetch_all($result,MYSQLI_ASSOC);
 <html>
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="shortcut icon" sizes="300x300" href="../images/kod_wiz_logo_org.png">
     <title>Tutorials</title>
     <link rel='stylesheet' href='./style.css' />
